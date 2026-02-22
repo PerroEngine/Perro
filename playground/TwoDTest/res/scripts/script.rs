1cd87533125867e6d9df1f49e40e603fe0ee498b
Tiernan DeFranco <supert422@gmail.com>
2026-02-21 19:02:57 -0800

    frontend rust scripts fully work

--- playground/TwoDTest/res/scripts/script.rs
+++ playground/TwoDTest/res/scripts/script.rs
@@ -1,14 +1,18 @@
 use perro_context::prelude::*;
 use perro_core::prelude::*;
 use perro_ids::prelude::*;
-use perro_modules::prelude::*;
+use perro_modules::{log, prelude::*};
 use perro_scripting::prelude::*;
 
 type SelfNodeType = Node2D;
 
 #[State]
 pub struct ExampleState {
+    #[default = 50.0]
     speed: f32,
+
+    #[default = true]
+    bool_var: bool,
 }
 
 ///@Script
@@ -18,11 +22,13 @@ impl<R: RuntimeAPI + ?Sized> ScriptLifecycle<R> for ExampleScript {
     fn on_init(&self, ctx: &mut RuntimeContext<'_, R>, self_id: NodeID) {
         let _origin = Vector2::new(0.0, 0.0);
         log_info!("Script initialized!");
-        let _ = ctx
+        let b = ctx
             .Scripts()
-            .with_state_mut::<ExampleState, _, _>(self_id, |state| {
-                state.speed = 240.0;
-            });
+            .with_state::<ExampleState, _, _>(self_id, |state| {
+                state.bool_var
+            }).unwrap_or_default();
+
+        log_info!(b);
     }
 
     fn on_update(&self, ctx: &mut RuntimeContext<'_, R>, self_id: NodeID) {
