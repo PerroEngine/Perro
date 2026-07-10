b555adea342a849ddbdc357db38e02a9b5d2d644
Tiernan DeFranco <supert422@gmail.com>
2026-07-10 07:47:42 -0700

    Remove KeyCode name format churn

--- perro_source/api_modules/perro_input_api/src/keycode.rs
+++ perro_source/api_modules/perro_input_api/src/keycode.rs
@@ -197,6 +197,203 @@ pub enum KeyCode {
     F35 = 193,
 }
 
+const KEY_NAMES: [&str; KeyCode::COUNT] = [
+    "Backquote",
+    "Backslash",
+    "BracketLeft",
+    "BracketRight",
+    "Comma",
+    "Digit0",
+    "Digit1",
+    "Digit2",
+    "Digit3",
+    "Digit4",
+    "Digit5",
+    "Digit6",
+    "Digit7",
+    "Digit8",
+    "Digit9",
+    "Equal",
+    "IntlBackslash",
+    "IntlRo",
+    "IntlYen",
+    "KeyA",
+    "KeyB",
+    "KeyC",
+    "KeyD",
+    "KeyE",
+    "KeyF",
+    "KeyG",
+    "KeyH",
+    "KeyI",
+    "KeyJ",
+    "KeyK",
+    "KeyL",
+    "KeyM",
+    "KeyN",
+    "KeyO",
+    "KeyP",
+    "KeyQ",
+    "KeyR",
+    "KeyS",
+    "KeyT",
+    "KeyU",
+    "KeyV",
+    "KeyW",
+    "KeyX",
+    "KeyY",
+    "KeyZ",
+    "Minus",
+    "Period",
+    "Quote",
+    "Semicolon",
+    "Slash",
+    "AltLeft",
+    "AltRight",
+    "Backspace",
+    "CapsLock",
+    "ContextMenu",
+    "ControlLeft",
+    "ControlRight",
+    "Enter",
+    "SuperLeft",
+    "SuperRight",
+    "ShiftLeft",
+    "ShiftRight",
+    "Space",
+    "Tab",
+    "Convert",
+    "KanaMode",
+    "Lang1",
+    "Lang2",
+    "Lang3",
+    "Lang4",
+    "Lang5",
+    "NonConvert",
+    "Delete",
+    "End",
+    "Help",
+    "Home",
+    "Insert",
+    "PageDown",
+    "PageUp",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowUp",
+    "NumLock",
+    "Numpad0",
+    "Numpad1",
+    "Numpad2",
+    "Numpad3",
+    "Numpad4",
+    "Numpad5",
+    "Numpad6",
+    "Numpad7",
+    "Numpad8",
+    "Numpad9",
+    "NumpadAdd",
+    "NumpadBackspace",
+    "NumpadClear",
+    "NumpadClearEntry",
+    "NumpadComma",
+    "NumpadDecimal",
+    "NumpadDivide",
+    "NumpadEnter",
+    "NumpadEqual",
+    "NumpadHash",
+    "NumpadMemoryAdd",
+    "NumpadMemoryClear",
+    "NumpadMemoryRecall",
+    "NumpadMemoryStore",
+    "NumpadMemorySubtract",
+    "NumpadMultiply",
+    "NumpadParenLeft",
+    "NumpadParenRight",
+    "NumpadStar",
+    "NumpadSubtract",
+    "Escape",
+    "Fn",
+    "FnLock",
+    "PrintScreen",
+    "ScrollLock",
+    "Pause",
+    "BrowserBack",
+    "BrowserFavorites",
+    "BrowserForward",
+    "BrowserHome",
+    "BrowserRefresh",
+    "BrowserSearch",
+    "BrowserStop",
+    "Eject",
+    "LaunchApp1",
+    "LaunchApp2",
+    "LaunchMail",
+    "MediaPlayPause",
+    "MediaSelect",
+    "MediaStop",
+    "MediaTrackNext",
+    "MediaTrackPrevious",
+    "Power",
+    "Sleep",
+    "AudioVolumeDown",
+    "AudioVolumeMute",
+    "AudioVolumeUp",
+    "WakeUp",
+    "Meta",
+    "Hyper",
+    "Turbo",
+    "Abort",
+    "Resume",
+    "Suspend",
+    "Again",
+    "Copy",
+    "Cut",
+    "Find",
+    "Open",
+    "Paste",
+    "Props",
+    "Select",
+    "Undo",
+    "Hiragana",
+    "Katakana",
+    "F1",
+    "F2",
+    "F3",
+    "F4",
+    "F5",
+    "F6",
+    "F7",
+    "F8",
+    "F9",
+    "F10",
+    "F11",
+    "F12",
+    "F13",
+    "F14",
+    "F15",
+    "F16",
+    "F17",
+    "F18",
+    "F19",
+    "F20",
+    "F21",
+    "F22",
+    "F23",
+    "F24",
+    "F25",
+    "F26",
+    "F27",
+    "F28",
+    "F29",
+    "F30",
+    "F31",
+    "F32",
+    "F33",
+    "F34",
+    "F35",
+];
+
 impl KeyCode {
     pub const COUNT: usize = 194;
     #[inline]
@@ -214,9 +411,15 @@ impl KeyCode {
             "KeyRight" => "ArrowRight",
             other => other,
         };
-        (0..Self::COUNT)
-            .filter_map(Self::from_index)
-            .find(|key| format!("{key:?}") == name)
+        KEY_NAMES
+            .iter()
+            .position(|key_name| *key_name == name)
+            .and_then(Self::from_index)
+    }
+
+    #[inline]
+    pub const fn as_name(self) -> &'static str {
+        KEY_NAMES[self.as_index()]
     }
 
     #[inline]
@@ -227,5 +430,29 @@ impl KeyCode {
         } else {
             None
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn every_key_name_roundtrips() {
+        for index in 0..KeyCode::COUNT {
+            let key = KeyCode::from_index(index).unwrap();
+            assert_eq!(KeyCode::from_name(key.as_name()), Some(key));
+        }
+    }
+
+    #[test]
+    fn legacy_aliases_resolve_without_allocating() {
+        assert_eq!(KeyCode::from_name("KeySpace"), Some(KeyCode::Space));
+        assert_eq!(KeyCode::from_name("KeyUp"), Some(KeyCode::ArrowUp));
+        assert_eq!(KeyCode::from_name("KeyDown"), Some(KeyCode::ArrowDown));
+        assert_eq!(KeyCode::from_name("KeyLeft"), Some(KeyCode::ArrowLeft));
+        assert_eq!(KeyCode::from_name("KeyRight"), Some(KeyCode::ArrowRight));
+        assert_eq!(KeyCode::from_name("  Space  "), Some(KeyCode::Space));
+        assert_eq!(KeyCode::from_name("space"), None);
     }
 }
